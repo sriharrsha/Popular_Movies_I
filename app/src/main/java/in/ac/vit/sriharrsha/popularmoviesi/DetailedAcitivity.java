d3374e2989cc0da33bc86d9d80536e0015739cf8
Sri Harrsha <sriharrsha@live.com>
2016-01-30 22:41:58 +0530

    Inserted Detailed Activity Made the options selected by users persisted through SharedPreferences

---
+++ app/src/main/java/in/ac/vit/sriharrsha/popularmoviesi/DetailedAcitivity.java
@@ -0,0 +1,13 @@
+package in.ac.vit.sriharrsha.popularmoviesi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailedAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_movie);
+    }
+}
